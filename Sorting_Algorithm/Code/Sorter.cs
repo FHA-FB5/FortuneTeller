4721173a2a69233a62c5759b3714119d1073b15d
Heratexx <dennis.koob@hotmail.de>
2018-08-17 19:48:43 +0200

    Sorter funktionalität
    Hinzugefügte Klassen: Member & Group

--- Sorting_Algorithm/Code/Sorter.cs
+++ Sorting_Algorithm/Code/Sorter.cs
@@ -7,5 +7,29 @@ namespace Sorting_Algorithm.Code
 {
     public class Sorter
     {
+        private List<Group> groups;
+
+        public Sorter() { }
+
+        //Fill group list
+        ///TODO Database based
+        public void ReadGroups() { }
+
+        private Group FindBestGroup()
+        {
+            ///TODO Verwende CalculateScore um besten Score zu fnden
+            ///TODO Gebe die Gruppe mit dem besten Score zurück
+            return new Group();
+        }
+        
+        public Group SortMemberIntoGroup(Member member)
+        {
+            ///TODO Finde beste Grupper 
+            ///TODO Schreibe member in die Gruppe ein (DB!)
+            ///TODO Übergebe Gruppenname an model für Outputview
+            
+            return new Group();
+        }
+
     }
 }

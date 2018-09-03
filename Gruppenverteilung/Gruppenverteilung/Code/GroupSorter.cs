6545bd6c082df482c78d54f5f694f132ac545a6c
Heratexx <dennis.koob@hotmail.de>
2018-09-03 23:34:24 +0200

    Gruppeneinteilung ausgearbeitet & Logger implementiert
    Gruppeneinteilung: Betrachtet alle eingegebene Informationen außer das Alter!
    Logger: Gibt die Möglichkeit eine Zeile zu loggen, soll vorerst verwendet werden um hinzufügen von Mitglieder und veränderte Raten in den Gruppen zu tracken.

--- Gruppenverteilung/Gruppenverteilung/Code/GroupSorter.cs
+++ Gruppenverteilung/Gruppenverteilung/Code/GroupSorter.cs
@@ -28,46 +28,275 @@ namespace Gruppenverteilung.Code
             Tutors.Add(new Tutor("TestTutor4", Studiengang.Informatik));
             Tutors.Add(new Tutor("TestTutor5", Studiengang.Informatik));
             groupscores = new List<double>();
-        } 
+        }
+        public double AverageMemberCount
+        {
+            get
+            {
+                double averge = 0.0;
 
+                foreach (Group group in Groups)
+                {
+                    averge += group.MemberList.Count;
+                }
+
+                return averge / Groups.Count;
+            }
+            set { AverageMemberCount = value; }
+        }
+        public double AverageMaleRate
+        {
+            get
+            {
+                double averge = 0.0;
+
+                foreach (Group group in Groups)
+                {
+                    averge += group.GenderRates.FirstOrDefault(kvp => kvp.Key == Geschlecht.Maennlich).Value;
+                }
+
+                return averge / Groups.Count;
+            }
+            set { AverageMaleRate = value; }
+        }
+        public double AverageFemaleRate
+        {
+            get
+            {
+                double averge = 0.0;
+
+                foreach (Group group in Groups)
+                {
+                    averge += group.GenderRates.FirstOrDefault(kvp => kvp.Key == Geschlecht.Weiblich).Value;
+                }
+
+                return averge / Groups.Count;
+            }
+            set { AverageFemaleRate = value; }
+        }
+        public double AverageMCDRate
+        {
+            get
+            {
+                double averge = 0.0;
+
+                foreach (Group group in Groups)
+                {
+                    averge += group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.MCD).Value;
+                }
+
+                return averge / Groups.Count;
+            }
+            set { AverageMaleRate = value; }
+        }
+        public double AverageINFRate
+        {
+            get
+            {
+                double averge = 0.0;
+
+                foreach (Group group in Groups)
+                {
+                    averge += group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.Informatik).Value;
+                }
+
+                return averge / Groups.Count;
+            }
+            set { AverageINFRate = value; }
+        }
+        public double AverageWINFRate
+        {
+            get
+            {
+                double averge = 0.0;
+
+                foreach (Group group in Groups)
+                {
+                    averge += group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.Wirtschaftsinformatik).Value;
+                }
+
+                return averge / Groups.Count;
+            }
+            set { AverageWINFRate = value; }
+        }
+        public double AverageETECHRate
+        {
+            get
+            {
+                double averge = 0.0;
+
+                foreach (Group group in Groups)
+                {
+                    averge += group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.Elektrotechnik).Value;
+                }
+
+                return averge / Groups.Count;
+            }
+            set { AverageETECHRate = value; }
+        }        
+       
         //Fill group list
         ///TODO Database based
         public void ReadGroups() { }
         public void ReadTutors() { }
+        //public Group FindBestGroup(Member member)
+        //{
+        //    Group BestGroup = FindNextEmptyGroup();
+        //    if (BestGroup == null)
+        //    {
+        //        double WorstMemberCourseRateInGroup = Groups[0].CourseRates.FirstOrDefault(kvp => kvp.Key == member.Studiengang).Value;
+        //        foreach (Group group in Groups)
+        //        {
+        //            double GroupCourseRate = group.CourseRates.FirstOrDefault(kvp => kvp.Key == member.Studiengang).Value;
+        //            if (GroupCourseRate == WorstMemberCourseRateInGroup)
+        //            {
+        //                if (BestGroup != null && BestGroup.MemberList.Count > group.MemberList.Count)
+        //                {
+        //                    BestGroup = group;
+        //                    WorstMemberCourseRateInGroup = group.CourseRates.FirstOrDefault(kvp => kvp.Key == member.Studiengang).Value;
+        //                }
+        //                else if (BestGroup == null)
+        //                {
+        //                    BestGroup = group;
+        //                    WorstMemberCourseRateInGroup = group.CourseRates.FirstOrDefault(kvp => kvp.Key == member.Studiengang).Value;
+        //                }                       
+        //            }
+        //            else if (GroupCourseRate < WorstMemberCourseRateInGroup)
+        //            {
+        //                BestGroup = group;
+        //                WorstMemberCourseRateInGroup = group.CourseRates.FirstOrDefault(kvp => kvp.Key == member.Studiengang).Value;
+        //            }
+        //        }
+        //    }
+
+        //    ///TODO Eventuell von ausßerhalb, da die methode FINDE beste gruppe heißt. (oder umbennen zu HINZUFÜGEN zu beste gruppe).
+        //    BestGroup.AddMember(member);
+
+        //    return BestGroup;
+        //}
+
         public Group FindBestGroup(Member member)
         {
-            Group BestGroup = FindNextEmptyGroup();
-            if (BestGroup == null)
+            //We haven't filtered any groups yet.
+            List<Group> FilterdGroups = GlobalVariables.Sorter.Groups;
+
+            FilterdGroups = GetLowestMemberGroupsFromGroupList(FilterdGroups);
+            FilterdGroups = GetLowestGenderRateFromGroupList(FilterdGroups, member.Geschlecht);
+            FilterdGroups = GetLowestCourseRateFromGroupList(FilterdGroups, member.Studiengang);
+
+            //TODO Auslagern
+            FilterdGroups[0].AddMember(member);
+
+            return FilterdGroups[0];
+        }
+
+        private List<Group> GetLowestMemberGroupsFromGroupList(List<Group> grouplist)
+        {
+            /*
+             What is a low member Group?:
+                |--------|--------|
+             Most     avarage   Lowest
+             Member   Member    Member
+                         |--------|
+                             Low
+                            Member
+             */
+            //Hold all low member groups in the end.
+            List<Group> FilterdGroups = new List<Group>();
+
+            foreach (Group group in grouplist)
             {
-                double WorstMemberCourseRateInGroup = Groups[0].CourseRates.FirstOrDefault(kvp => kvp.Key == member.Studiengang).Value;
-                foreach (Group group in Groups)
+                //If Groups membercount is lesser than the AverageCount
+                if (group.MemberList.Count <= this.AverageMemberCount)
                 {
-                    double GroupCourseRate = group.CourseRates.FirstOrDefault(kvp => kvp.Key == member.Studiengang).Value;
-                    if (GroupCourseRate == WorstMemberCourseRateInGroup)
-                    {
-                        if (BestGroup != null && BestGroup.MemberList.Count > group.MemberList.Count)
-                        {
-                            BestGroup = group;
-                            WorstMemberCourseRateInGroup = group.CourseRates.FirstOrDefault(kvp => kvp.Key == member.Studiengang).Value;
-                        }
-                        else if (BestGroup == null)
-                        {
-                            BestGroup = group;
-                            WorstMemberCourseRateInGroup = group.CourseRates.FirstOrDefault(kvp => kvp.Key == member.Studiengang).Value;
-                        }                       
-                    }
-                    else if (GroupCourseRate < WorstMemberCourseRateInGroup)
-                    {
-                        BestGroup = group;
-                        WorstMemberCourseRateInGroup = group.CourseRates.FirstOrDefault(kvp => kvp.Key == member.Studiengang).Value;
-                    }
+                    FilterdGroups.Add(group);
                 }
             }
 
-            ///TODO Eventuell von ausßerhalb, da die methode FINDE beste gruppe heißt. (oder umbennen zu HINZUFÜGEN zu beste gruppe).
-            BestGroup.AddMember(member);
+            return (FilterdGroups.Count == 0) ? grouplist : FilterdGroups;
+        }
+        private List<Group> GetLowestGenderRateFromGroupList(List<Group> grouplist, Geschlecht gender)
+        {
+            /*
+             What is a low gender rate Group?:
+                |--------|--------|
+             Highest   Avarage   Lowest
+             Gender    Gender    Gender
+             Rate      Rate      Rate
+                         |--------|
+                             Low
+                            Gender
+                             Rates
+             */
 
-            return BestGroup;
+            //Hold all low gender rate groups in the end.
+            List<Group> FilterdGroups = new List<Group>();
+            double GroupGenderRate = 0.0;
+            double AverageGenderRate = 0.0;
+            foreach (Group group in grouplist)
+            {
+                //Searching for the right Rates
+                GroupGenderRate = (gender == Geschlecht.Maennlich) ? group.GenderRates.FirstOrDefault(kvp => kvp.Key == Geschlecht.Maennlich).Value : group.GenderRates.FirstOrDefault(kvp => kvp.Key == Geschlecht.Weiblich).Value;
+                AverageGenderRate = (gender == Geschlecht.Maennlich) ? AverageMaleRate : AverageFemaleRate;
+
+                //If Groups Genderrate is lesser than the AverageCount, add to filterd Groups
+                if (GroupGenderRate <= AverageGenderRate)
+                {
+                    FilterdGroups.Add(group);
+                }
+            }
+
+            return (FilterdGroups.Count == 0) ? grouplist : FilterdGroups;
+        }
+        private List<Group> GetLowestCourseRateFromGroupList(List<Group> grouplist, Studiengang course)
+        {
+            /*
+             What is a low gender rate Group?:
+                |--------|--------|
+             Highest   Avarage   Lowest
+             Course    Course    Course
+             Rate      Rate      Rate
+                         |--------|
+                             Low
+                            Course
+                             Rates
+             */
+
+            //Hold all low course rate groups in the end.
+            List<Group> FilterdGroups = new List<Group>();
+
+            double GroupeCourseRate = 0.0;
+            double AverageCourseRate = 0.0;
+            foreach (Group group in grouplist)
+            {
+                //Searching for the right Rates
+                switch (course)
+                {
+                    case Studiengang.MCD:
+                        GroupeCourseRate = group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.MCD).Value;
+                        AverageCourseRate = AverageMCDRate;
+                        break;
+                    case Studiengang.Informatik:
+                        GroupeCourseRate = group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.Informatik).Value;
+                        AverageCourseRate = AverageINFRate;
+                        break;
+                    case Studiengang.Wirtschaftsinformatik:
+                        GroupeCourseRate = group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.Wirtschaftsinformatik).Value;
+                        AverageCourseRate = AverageWINFRate;
+                        break;
+                    case Studiengang.Elektrotechnik:
+                        GroupeCourseRate = group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.Elektrotechnik).Value;
+                        AverageCourseRate = AverageETECHRate;
+                        break;
+                }
+                //If Groups courserate is lesser than the AverageCourseRate
+                if (GroupeCourseRate <= AverageCourseRate)
+                {
+                    FilterdGroups.Add(group);
+                }
+            }
+
+            return (FilterdGroups.Count == 0) ? grouplist : FilterdGroups;
         }
 
         private Group FindNextEmptyGroup()
@@ -105,7 +334,25 @@ namespace Gruppenverteilung.Code
                     gender = Geschlecht.Weiblich;
 
                 Member member = new Member(splittedLine[0], Convert.ToInt16(splittedLine[1]), stdgang, gender);
-                FindBestGroup(member);
+                Group BestGroup = FindBestGroup(member);
+
+                //LOGGING: Added Member Log
+                GlobalVariables.TestLogger.LogLine(String.Format("(Member Added) -> {0} ({1}), {2}, {3} - in Group \"{4}\"", member.Name, member.Age, member.Studiengang.ToString(), member.Geschlecht.ToString(), BestGroup.Name));
+
+                //LOGGING: Group-State Log
+                foreach (Group group in GlobalVariables.Sorter.Groups)
+                {
+                    GlobalVariables.TestLogger.LogLine(String.Format("(Group State) -> GroupName: {0}, MemberCount: {1}, Male: {2:N2}%, Female: {3:N2}%, MCD: {4:N2}%, INF: {5:N2}%, WINF: {6:N2}%, ETECH: {7:N2}% ",
+                        group.Name,
+                        group.MemberList.Count,
+                        group.GenderRates.FirstOrDefault(kvp => kvp.Key == Geschlecht.Weiblich).Value * 100,
+                        group.GenderRates.FirstOrDefault(kvp => kvp.Key == Geschlecht.Maennlich).Value * 100,
+                        group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.MCD).Value * 100,
+                        group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.Informatik).Value * 100,
+                        group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.Wirtschaftsinformatik).Value * 100,
+                        group.CourseRates.FirstOrDefault(kvp => kvp.Key == Studiengang.Elektrotechnik).Value * 100
+                        ));
+                }
             }
 
         }
---
+++ Gruppenverteilung/Gruppenverteilung/Code/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Gruppenverteilung.Code
+{
+    public class Logger
+    {
+        #region Probs...
+        public string FileName { get; private set; }
+        public StreamWriter Writer { get; private set; }
+        #endregion
+
+        #region Constructors...
+        public Logger(string filename)
+        {
+            FileName = filename;
+        }
+        #endregion
+
+        #region Public methods...
+        public void LogLine(string value)
+        {
+            ///new StreaWriter(filename, append)
+            Writer = new StreamWriter(FileName, true);
+            ///LogFormat: " [Date, Time]: Linevalue  "
+            Writer.WriteLine(String.Format("[{0}, {1}]: {2}", DateTime.Now.ToShortDateString(), DateTime.Now.ToLongTimeString(), value));
+            Writer.Close();
+        }
+        #endregion
+
+        #region Private methods..
+
+        #endregion
+    }
+}

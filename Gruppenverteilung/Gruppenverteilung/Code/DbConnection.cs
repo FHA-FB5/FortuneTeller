bdc78d4ff72fef990b3de152263cf76a1c939d9d
Heratexx <dennis.koob@hotmail.de>
2018-09-12 19:32:08 +0200

    Datenbankfunktionen erweitert
    Member könne in die Datenbank geschrieben werden.
    Member können gruppen über MemberID und GroupID zugewiesen werden.

--- Gruppenverteilung/Gruppenverteilung/Code/DbConnection.cs
+++ Gruppenverteilung/Gruppenverteilung/Code/DbConnection.cs
@@ -18,6 +18,45 @@ namespace Gruppenverteilung.Code
             Connection = new SqlConnection(ConnectionString);
         }
 
+        #region GetData...
+        public int GetGroupIDByName(string groupname)
+        {
+            int id = -1;
+            Connection.Open();
+            using (SqlCommand command = new SqlCommand(String.Format(@"SELECT GroupId FROM dbo.tbl_Group WHERE Name = '{0}'", groupname), Connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        id = Convert.ToInt32(reader["GroupId"]);
+                    }
+                }
+            }
+            Connection.Close();
+
+            return id;
+        }
+
+        public int GetLastMemberID()
+        {
+            int id = -1;
+            Connection.Open();
+            using (SqlCommand command = new SqlCommand(@"SELECT TOP 1 MemberId FROM dbo.tbl_Member ORDER BY Memberid DESC", Connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        id = Convert.ToInt32(reader["MemberId"]);
+                    }
+                }
+            }
+            Connection.Close();
+
+            return id;
+        }    
+
         public List<Group> GetAllGroups()
         {
             List<Group> GroupsFromDatabase = new List<Group>();
@@ -39,6 +78,55 @@ namespace Gruppenverteilung.Code
             return GroupsFromDatabase;
         }
 
+        public List<Member> GetAllMember()
+        {
+            List<Member> MemberFromDatabase = new List<Member>();
+            Connection.Open();
+            using (SqlCommand command = new SqlCommand(@"SELECT * FROM dbo.tbl_Member;", Connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        //TODO: Auslagern
+                        Geschlecht gender;
+                        if (reader["Gender"].ToString() == Geschlecht.Maennlich.ToString())
+                        {
+                            gender = Geschlecht.Maennlich;
+                        }
+                        else
+                        {
+                            gender = Geschlecht.Weiblich;
+                        }
+                        Studiengang course;
+                        if (reader["Course"].ToString() == Studiengang.Elektrotechnik.ToString())
+                        {
+                            course = Studiengang.Elektrotechnik;
+                        }
+                        else if(reader["Course"].ToString() == Studiengang.Informatik.ToString())
+                        {
+                            course = Studiengang.Informatik;
+                        }
+                        else if (reader["Course"].ToString() == Studiengang.Wirtschaftsinformatik.ToString())
+                        {
+                            course = Studiengang.Wirtschaftsinformatik;
+                        }
+                        else
+                        {
+                            course = Studiengang.MCD;
+                        }
+                        Member member = new Member(reader["FirstName"].ToString(), Convert.ToInt32(reader["Age"]), course, gender);
+                        MemberFromDatabase.Add(member);
+                    }
+                }
+            }
+            Connection.Close();
+
+            return MemberFromDatabase;
+        }
+        #endregion
+
+        #region Insert...
         /// <summary>
         /// Insert a group into the database
         /// </summary>
@@ -61,5 +149,64 @@ namespace Gruppenverteilung.Code
 
             return AffectedRows;
         }
+
+        /// <summary>
+        /// Insert a Member into the database
+        /// </summary>
+        /// <param name="firstname"></param>
+        /// <param name="lastname"></param>
+        /// <param name="age"></param>
+        /// <param name="gender"></param>
+        /// <param name="course"></param>
+        /// <returns>Affected rows, should be one by default.</returns>
+        public int InsertMember(string firstname, string lastname, int age, Geschlecht gender, Studiengang course)
+        {
+            Connection.Open();
+            int AffectedRows = 0;
+            using (SqlCommand command = new SqlCommand(@"INSERT INTO [dbo].[tbl_Member]([FirstName], [LastName], [Age], [Gender], [Course]) VALUES(@firstname,  @lastname, @age, @gender, @course);", Connection))
+            {
+                command.Parameters.Add("@firstname", SqlDbType.NChar);
+                command.Parameters["@firstname"].Value = firstname;
+                command.Parameters.Add("@lastname", SqlDbType.NChar);
+                command.Parameters["@lastname"].Value = lastname;
+                command.Parameters.Add("@age", SqlDbType.Int);
+                command.Parameters["@age"].Value = age;
+                command.Parameters.Add("@gender", SqlDbType.NChar);
+                command.Parameters["@gender"].Value = gender.ToString();
+                command.Parameters.Add("@course", SqlDbType.NChar);
+                command.Parameters["@course"].Value = course.ToString();
+
+                AffectedRows = command.ExecuteNonQuery();
+            }
+            Connection.Close();
+
+            return AffectedRows;
+        }
+
+        /// <summary>
+        /// Assign a Member to a group.
+        /// </summary>
+        /// <param name="memberid">Database MemberID</param>
+        /// <param name="groupid">Database GroupID</param>
+        /// <returns></returns>
+        public int AssignMemberToGroup(int memberid, int groupid)
+        {
+            Connection.Open();
+            int AffectedRows = 0;
+            using (SqlCommand command = new SqlCommand(@"INSERT INTO [dbo].[tbl_GroupMember]([MemberId], [GroupId]) VALUES(@memberid,  @groupid);", Connection))
+            {
+                command.Parameters.Add("@memberid", SqlDbType.Int);
+                command.Parameters["@memberid"].Value = memberid;
+                command.Parameters.Add("@groupid", SqlDbType.Int);
+                command.Parameters["@groupid"].Value = groupid;
+
+                AffectedRows = command.ExecuteNonQuery();
+            }
+            Connection.Close();
+
+            return AffectedRows;
+        }
+        #endregion
+
     }
 }

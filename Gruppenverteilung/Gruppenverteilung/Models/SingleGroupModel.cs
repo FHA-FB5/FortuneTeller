4733d323deed2411e19c3a7956254a4b6c0870e0
Emptii <max.con.96@gmail.com>
2018-09-20 11:59:43 +0200

    Gruppe Drucken hinzugefügt

---
+++ Gruppenverteilung/Gruppenverteilung/Models/SingleGroupModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gruppenverteilung.Code;
+
+namespace Gruppenverteilung.Models
+{
+    public class SingleGroupModel
+    {
+        public Group Group { get; set; }
+
+        public SingleGroupModel()
+        {
+
+        }
+
+        public SingleGroupModel(Group group)
+        {
+            Group = group;
+        }
+    }
+}

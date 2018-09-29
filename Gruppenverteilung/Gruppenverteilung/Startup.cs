9e8340d3ef2adb311391dfeaa59be678c0149226
Emptii <max.con.96@gmail.com>
2018-09-29 12:32:50 +0200

    Semicolons weggemacht

--- Gruppenverteilung/Gruppenverteilung/Startup.cs
+++ Gruppenverteilung/Gruppenverteilung/Startup.cs
@@ -57,15 +57,16 @@ namespace Gruppenverteilung
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();

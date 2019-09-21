707be3868e07e6751857ff3a464d9a08194ca98d
Knut Zuidema <me@knutzuidema.de>
2019-09-21 14:02:15 +0200

    feat(web/src): add config

---
+++ web/src/config.ts
@@ -0,0 +1,7 @@
+export let config = {
+  apiUrl: "http://127.0.0.1:5000",
+  sourceUrl: "https://github.com/FHA-FB5/FortuneTeller",
+  projectName: "Fortune Teller",
+  copyrightUrl: "https://fsr5.de",
+  copyrightOwner: "FH Aachen Fachbereich 5"
+}

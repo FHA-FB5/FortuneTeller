10c757c9480734064a6674c1f1f3d76ab1877bb0
Knut Zuidema <me@knutzuidema.de>
2019-09-27 23:16:46 +0200

    feat(cmd/groups_overview): add group overview server

---
+++ cmd/groups_overview/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/FHA-FB5/FortuneTeller/pkg/model"
+)
+
+func main() {
+	var (
+		address    = flag.String("ip", "0.0.0.0", "ip address for server")
+		port       = flag.Int("port", 8080, "port for server")
+		apiAddress = flag.String("api", "http://0.0.0.0:5000", "api address")
+	)
+	flag.Parse()
+	groupTmpl, err := ioutil.ReadFile("templates/group.gohtml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	overviewTmpl, err := ioutil.ReadFile("templates/overview.gohtml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	groupPage := template.Must(template.New("group").Parse(string(groupTmpl)))
+	overviewPage := template.Must(template.New("overview").Parse(string(overviewTmpl)))
+	router := mux.NewRouter()
+	router.Path("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		response, err := http.Get(*apiAddress + "/groups")
+		if err != nil {
+			http.Error(w, "can not get groups", http.StatusInternalServerError)
+			return
+		}
+		var groups []*model.Group
+		if err := json.NewDecoder(response.Body).Decode(&groups); err != nil {
+			http.Error(w, "can not decode response", http.StatusInternalServerError)
+			return
+		}
+		if err := overviewPage.Execute(w, groups); err != nil {
+			http.Error(w, "can not execute template", http.StatusInternalServerError)
+		}
+	})
+	router.Path("/{groupID}").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		groupId, ok := mux.Vars(r)["groupID"]
+		if !ok {
+			http.Error(w, "no group id supplied", http.StatusBadRequest)
+			return
+		}
+		response, err := http.Get(*apiAddress + "/group/" + groupId)
+		if err != nil {
+			http.Error(w, "can not get group", http.StatusInternalServerError)
+			return
+		}
+		var group model.Group
+		if err := json.NewDecoder(response.Body).Decode(&group); err != nil {
+			http.Error(w, "can not decode response body", http.StatusInternalServerError)
+			return
+		}
+		if err := groupPage.Execute(w, group); err != nil {
+			http.Error(w, "could not execute template", http.StatusInternalServerError)
+			return
+		}
+	})
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", *address, *port), router); err != nil {
+		log.Fatal(err)
+	}
+}

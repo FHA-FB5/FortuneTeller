4179596d22a0fcd02e0e30b8b99834206c9f852f
Knut Zuidema <me@knutzuidema.de>
2019-09-30 02:48:06 +0200

    view group

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,5 +1,6 @@
 export let config = {
   apiUrl: "http://127.0.0.1:5000",
+  overviewUrl: "http://127.0.0.1:8080",
   sourceUrl: "https://github.com/FHA-FB5/FortuneTeller",
   projectName: "Fortune Teller",
   copyrightUrl: "https://fsr5.de",
